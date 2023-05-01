fbea692bd79c305327ddf202bb97b5a2d9b14025
MQ <3524982+mqzhang@users.noreply.github.com>
2023-05-01 09:29:52 +0800

    Add docker file

---
+++ init.sh
@@ -0,0 +1,43 @@
+# 使用清华源 for ubuntu 22.04 jammy 
+# https://mirror.tuna.tsinghua.edu.cn/help/ubuntu/
+cat > /etc/apt/sources.list << EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
+
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+EOF
+
+apt-get update --yes && \
+    apt-get install --yes --no-install-recommends \
+    ruby-full \
+    build-essential autoconf libtool \
+    git \
+    sqlite3 libsqlite3-dev \
+    screen && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
+
+# 添加镜像源并移除默认源
+gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove https://rubygems.org/
+# 列出已有源
+gem sources -l
+# 应该只有镜像源一个
+
+# echo installing current RubyGems
+gem update --system -N >/dev/null 2>&1
+
+echo installing Bundler
+gem install bundler -N >/dev/null 2>&1
