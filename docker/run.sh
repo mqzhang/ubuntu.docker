d06d5c54334a9913a05f74a84bfb6252d6465015
MQ <3524982+mqzhang@users.noreply.github.com>
2023-05-06 21:41:48 +0800

    update

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 set -x
 
-docker run -it --entrypoint /bin/bash -d --name mqdev -p 3001:3000 -v $HOME:/root/home quay.io/mqdev/ubuntu
+docker run -it --entrypoint /bin/bash -d --name mqdev -p 3001:3000 -v $HOME:/root/home -v $(pwd):/root/workdir quay.io/mqdev/ubuntu
 docker exec -it mqdev bash
