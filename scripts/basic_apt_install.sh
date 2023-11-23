22d2d7e787b0d69436fb54e3981ac0b8213ecf5d
mq <zmingqian@gmail.com>
2023-11-23 18:49:43 +0800

    update

--- scripts/basic_apt_install.sh
+++ scripts/basic_apt_install.sh
@@ -1,7 +1,7 @@
     set -x
     apt-get update --yes && \
     apt-get install --yes --no-install-recommends \
-    apt-utils build-essential autoconf libtool libssl-dev libffi-dev libyaml-dev \
+    apt-utils build-essential autoconf libtool libssl-dev libffi-dev libyaml-dev libzmq3-dev \
     zlib1g-dev libbz2-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev libffi-dev libreadline-dev \
     nodejs npm \
     git curl \
