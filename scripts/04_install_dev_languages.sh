6a05866c1c80b41e9c3ca4ffeec572a0055f356c
mqzhang <zmingqian@qq.com>
2025-01-28 00:06:47 +0800

    update

--- scripts/04_install_dev_languages.sh
+++ scripts/04_install_dev_languages.sh
@@ -1,14 +1,6 @@
 mise use --global ruby@3.3
 mise x ruby -- gem install rails --no-document
-echo 'gem: --no-document' >> ~/.gemrc
-gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
-bundle config mirror.https://rubygems.org https://gems.ruby-china.com
 
 mise use --global node@lts
-npm config set registry https://registry.npmmirror.com
-npm install --global yarn
-yarn config set registry https://registry.npmmirror.com
 
 mise use --global python@latest
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install -r requirements.txt
---
+++ scripts/05_config_dev_languages.sh
@@ -0,0 +1,12 @@
+mise activate
+
+echo 'gem: --no-document' >> ~/.gemrc
+gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
+bundle config mirror.https://rubygems.org https://gems.ruby-china.com
+
+npm config set registry https://registry.npmmirror.com
+npm install --global yarn
+yarn config set registry https://registry.npmmirror.com
+
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
