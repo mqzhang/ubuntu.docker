1da018251426b895493bf724504b8fa33dab8914
MQ <zmingqian@gmail.com>
2025-01-28 10:02:30 +0800

    update

--- scripts/05_config_dev_languages.sh
+++ scripts/05_config_dev_languages.sh
@@ -1,4 +1,5 @@
 eval "$(mise activate bash)"
+export PATH="${PATH}:/less/important/path:/lesser/important/path"
 
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt
@@ -7,6 +8,10 @@ echo 'gem: --no-document' >> ~/.gemrc
 gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
 bundle config mirror.https://rubygems.org https://gems.ruby-china.com
 bundle install
+
+# iruby
+# https://github.com/SciRuby/iruby
+gem install iruby
 iruby register --force
 
 
