3b8ce1d9c60d501006e8486ca470cdc3db367aee
MQ <zmingqian@gmail.com>
2025-01-28 09:13:48 +0800

    update

--- jupyter/start_jupyter.sh
+++ jupyter/start_jupyter.sh
@@ -1,4 +1,6 @@
-eval "$(~/.rbenv/bin/rbenv init - bash)" 
-eval "$(~/.pyenv/bin/pyenv init -)" 
+# eval "$(~/.rbenv/bin/rbenv init - bash)"
+# eval "$(~/.pyenv/bin/pyenv init -)"
+
+eval "$(mise activate bash)"
 
 jupyter lab --allow-root --config=/tmp/jupyter_lab_config.py
--- scripts/05_config_dev_languages.sh
+++ scripts/05_config_dev_languages.sh
@@ -1,12 +1,16 @@
 eval "$(mise activate bash)"
 
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
+
 echo 'gem: --no-document' >> ~/.gemrc
 gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
 bundle config mirror.https://rubygems.org https://gems.ruby-china.com
+bundle install
+iruby register --force
+
 
 npm config set registry https://registry.npmmirror.com
 npm install --global yarn
 yarn config set registry https://registry.npmmirror.com
 
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install -r requirements.txt
