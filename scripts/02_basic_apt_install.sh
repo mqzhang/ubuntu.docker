fb2b5e69e8fc6a6d792b814a2f8fba6d06f8df59
MQ <zmingqian@qq.com>
2025-05-16 07:36:12 +0800

    add pipx and uv

--- scripts/02_basic_apt_install.sh
+++ scripts/02_basic_apt_install.sh
@@ -13,5 +13,9 @@
     build-essential pkg-config autoconf bison clang rustc \
     libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
     libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
-    redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
+    redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common \
+    pipx
  
+    # use pipx to install uv Install pipx for managing Python applications
+    pipx ensurepath
+    pipx install uv    # Install pipx for managing Python applications
